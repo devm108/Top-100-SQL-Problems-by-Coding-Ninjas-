11cc24f50e1bf7f6098f2e123279c3301817c1f6
Dev Mittal <53218476+devm108@users.noreply.github.com>
2022-02-24 18:48:12 +0530

    Create Number of Calls between two persons.sql

---
+++ Number of Calls between two persons.sql
@@ -0,0 +1,6 @@
+select from_id as person1,
+to_id as person2,
+count(duration) as call_count,
+sum(duration) as total_duration from (
+select * from Calls Union All select to_id,from_id,duration from Calls) t1
+where from_id<to_id group by person1,person2

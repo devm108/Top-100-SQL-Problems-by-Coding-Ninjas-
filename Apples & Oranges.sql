e8795bee80542cf331057afd37d8258ed7fa88a2
Dev Mittal <53218476+devm108@users.noreply.github.com>
2022-03-02 16:50:18 +0530

    Create Apples & Oranges.sql

---
+++ Apples & Oranges.sql
@@ -0,0 +1,6 @@
+select date(sale_date) as sale_date,
+       sum(case when fruit = 'apples' then sold_num
+                when fruit = 'oranges' then -sold_num end) as diff
+from Sales
+group by 1
+order by 1
